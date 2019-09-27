b53a451e52cc380a0a65cfe89b338ac0e6ff9d4e
DennySPB <dennyspb@gmail.com>
2019-09-27 15:21:57 +0300

    Launcher3: Add toggle for DT2S
    ejbtrd: Adapt to T QPR2
    
    commit 39fa8b063cf682771c1800a2391689f96808218f
    Author: Omkar Chandorkar <gotenksIN@aosip.dev>
    Date:   Tue Feb 1 19:06:26 2022 +0530
    
        fixup! Launcher3: Add toggle for DT2S
    
        Change-Id: I2e537fa8641503d4ddfb10cad79e7d5fd0db3f2b
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I22abb02de7394094653d571e20eee0e93fb1e9a5
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -27,6 +27,7 @@ import android.app.ActivityManager;
 import android.app.Person;
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.LauncherActivityInfo;
 import android.content.pm.LauncherApps;
 import android.content.pm.PackageManager;
@@ -68,6 +69,7 @@ import androidx.annotation.ChecksSdkIntAtLeast;
 import androidx.annotation.NonNull;
 import androidx.core.graphics.ColorUtils;
 
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.dragndrop.FolderAdaptiveIcon;
 import com.android.launcher3.graphics.TintedDrawableSpan;
 import com.android.launcher3.icons.ShortcutCachingLogic;
@@ -127,6 +129,8 @@ public final class Utilities {
      * Set on a motion event dispatched from the nav bar. See {@link MotionEvent#setEdgeFlags(int)}.
      */
     public static final int EDGE_NAV_BAR = 1 << 8;
+
+    public static final String KEY_DT_GESTURE = "pref_dt_gesture";
 
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
@@ -734,6 +738,11 @@ public final class Utilities {
                 label, matrix, matrixValues[Matrix.MSCALE_X], matrixValues[Matrix.MSCALE_Y],
                 matrixValues[Matrix.MTRANS_X], matrixValues[Matrix.MTRANS_Y]
         ));
+    }
+
+    public static boolean isDoubleTapGestureEnabled(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DT_GESTURE, true);
     }
 
     public static boolean isGSAEnabled(Context context) {
