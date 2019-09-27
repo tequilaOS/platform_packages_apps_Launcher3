42fd13d452633e9e9d962f59f676363a6cb8f7aa
DennySPB <dennyspb@gmail.com>
2019-09-27 15:21:57 +0300

    Launcher3: Add toggle for DT2S
    commit 39fa8b063cf682771c1800a2391689f96808218f
    Author: Omkar Chandorkar <gotenksIN@aosip.dev>
    Date:   Tue Feb 1 19:06:26 2022 +0530
    
        fixup! Launcher3: Add toggle for DT2S
    
        Change-Id: I2e537fa8641503d4ddfb10cad79e7d5fd0db3f2b
    
    ejbtrd: Adapt to QPR2
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I22abb02de7394094653d571e20eee0e93fb1e9a5

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -27,6 +27,7 @@ import android.app.ActivityManager;
 import android.app.Person;
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.LauncherActivityInfo;
 import android.content.pm.LauncherApps;
 import android.content.pm.PackageManager;
@@ -127,6 +128,8 @@ public final class Utilities {
      * Set on a motion event dispatched from the nav bar. See {@link MotionEvent#setEdgeFlags(int)}.
      */
     public static final int EDGE_NAV_BAR = 1 << 8;
+
+    public static final String KEY_DT_GESTURE = "pref_dt_gesture";
 
     /**
      * Indicates if the device has a debug build. Should only be used to store additional info or
@@ -742,5 +745,10 @@ public final class Utilities {
         } catch (PackageManager.NameNotFoundException e) {
             return false;
         }
+    }
+
+    public static boolean isDoubleTapGestureEnabled(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DT_GESTURE, true);
     }
 }
